ad39fd8274e97a08e4962824ac1f5169a571c928
Łukasz Pęgiel <me@abapblog.com>
2020-07-20 19:34:52 +0200

    RemoveAllComments
    + some bugs

--- ABAP Quick Fix Plugin/src/com/abapblog/adt/quickfix/preferences/PreferenceConstants.java
+++ ABAP Quick Fix Plugin/src/com/abapblog/adt/quickfix/preferences/PreferenceConstants.java
@@ -5,5 +5,6 @@ public class PreferenceConstants {
 	public static final String P_TCTE_REMOVE_COMMENT_SIGNS = "tcteRemoveCommentSigns";
 	public static final String P_TCTE_REMOVE_LINE_BREAKS = "tcteRemoveLineBreaks";
 	public static final String P_TCTE_ALLOWED = "tcteAllowed";
+	public static final String HideRemoveAllComments = "hideRemoveAllComments";
 
 }
